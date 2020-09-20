4c3fd768be4c25a0ad772380842ae9ef5c055ed6
Rogfrich <richard.a.cairns@gmail.com>
2020-09-20 19:37:50 +0100

    create data for use in autogenerated readme.md

--- create_readme.py
+++ create_readme.py
@@ -1,10 +1,36 @@
 import os
 
-ignored_directories = ['docs', 'venv', '.git', '.idea', '.pytest_cache', '__pycache__']
+ignored_directories = [
+    "docs",
+    "venv",
+    ".git",
+    ".idea",
+    ".pytest_cache",
+    "__pycache__",
+    "test_category1",
+    "test_category2",
+]
 
 
 def get_categories():
     categories = [
-        x for x in os.listdir(".") if os.path.isdir(x) and x not in ignored_directories
+        category for category in os.listdir(".") if os.path.isdir(category) and category not in ignored_directories
     ]
     return sorted(categories)
+
+
+def get_data(categories: list):
+    data = {}
+    for category in categories:
+        articles_in_category = []
+        for file in sorted(os.listdir(category)):
+            with open(os.path.join(category, file)) as f:
+                for line in f:
+                    if line.startswith("#"):
+                        articles_in_category.append(
+                            (line[1:].strip(), os.path.join(category, file))
+                        )
+                        break
+        data[category] = articles_in_category
+    return data
+
--- test_create_readme.py
+++ test_create_readme.py
@@ -1,5 +1,6 @@
 import os
-from create_readme import get_categories, ignored_directories
+from create_readme import get_categories, ignored_directories, get_data
+
 
 def test_get_categories():
     categories = get_categories()
@@ -7,3 +8,25 @@ def test_get_categories():
     for item in categories:
         assert item in os.listdir(".") and item not in ignored_directories
 
+
+def test_get_data_returns_dict():
+    categories = get_categories()
+    assert isinstance(get_data(categories), dict)
+
+
+def test_get_data_excludes_test_directories():
+    categories = get_categories()
+    data = get_data(categories)
+    for i in ("test_category1", "test_category2"):
+        assert i not in data.keys()
+
+
+def test_get_data_returns_correct_values():
+    valid_data = {
+        "test_category1": [('CAT 1 - ARTICLE 1', 'test_category1/test_cat1_article1.md'), ('CAT 1 - ARTICLE 2', 'test_category1/test_cat1_article2.md')],
+        "test_category2": [('CAT 2 - ARTICLE 1', 'test_category2/test_cat2_article1.md'), ('CAT 2 - ARTICLE 2', 'test_category2/test_cat2_article2.md')],
+    }
+
+    test_categories = ["test_category1", "test_category2"]
+    data = get_data(test_categories)
+    assert data == valid_data
