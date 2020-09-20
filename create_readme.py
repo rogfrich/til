9f59f833e02220f957e0d651de94692985bf75a0
Rogfrich <richard.a.cairns@gmail.com>
2020-09-20 16:40:28 +0100

    get category folders

---
+++ create_readme.py
@@ -0,0 +1,10 @@
+import os
+
+ignored_directories = ['docs', 'venv', '.git', '.idea', '.pytest_cache', '__pycache__']
+
+
+def get_categories():
+    categories = [
+        x for x in os.listdir(".") if os.path.isdir(x) and x not in ignored_directories
+    ]
+    return categories
