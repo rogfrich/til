316617ad0bdfbb29f690904892541ccc8928f2b6
Rogfrich <richard.a.cairns@gmail.com>
2020-09-20 16:50:09 +0100

    get_categories returns sorted list

--- create_readme.py
+++ create_readme.py
@@ -7,4 +7,4 @@ def get_categories():
     categories = [
         x for x in os.listdir(".") if os.path.isdir(x) and x not in ignored_directories
     ]
-    return categories
+    return sorted(categories)
